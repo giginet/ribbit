4ef21dd84c579dbfd91b63b3a587e648599b785b
giginet <giginet.net@gmail.com>
2013-12-14 00:48:14 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ribbit',
+      version='0.1',
+      description='ribbit is the perfect collaboration tool for all creators',
+      author='giginet',
+      author_email='giginet.net@gmail.com',
+      url='https://github.com/giginet/ribbit',
+      requires=[
+        'django (>=1.6.0)'
+      ]
+)
