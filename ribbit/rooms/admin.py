cd3488136d403c95f81ffa6e2f641dd8a7e59a84
giginet <giginet.net@gmail.com>
2013-12-13 23:52:38 +0900

    Add room model.

--- ribbit/rooms/admin.py
+++ ribbit/rooms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Room
 
-# Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Room)
--- ribbit/rooms/models.py
+++ ribbit/rooms/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+u"""
+    Model for Char room.
+"""
+class Room(models.Model):
+
+    ROOM_SCOPE = (
+        ('public', _('Public')),
+        ('private', _('Invite Only')),
+    )
+
+    def icon_image_path(filename):
+        return ''
+
+    name = models.CharField(max_length=128, verbose_name=_('Room'), null=False, blank=False)
+    scope = models.CharField(choices=ROOM_SCOPE, verbose_name=_('Scope'), default='public', max_length=16)
+    icon_image = models.ImageField(verbose_name=_('Icon Image'), null=True, blank=True, upload_to=icon_image_path)
+    created_at = models.DateTimeField(auto_now=True, verbose_name=_('Created At'))
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Updated At'))
+
+    class Meta:
+        verbose_name = 'Room'
+        verbose_name_plural = 'Rooms'
