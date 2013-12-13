23d94c7a5f9af4de5eaec17c7e270eae0a1df061
giginet <giginet.net@gmail.com>
2013-12-14 07:49:54 +0900

    Modify runtests script

--- runtests.py
+++ runtests.py
@@ -1,16 +1,17 @@
 #This file mainly exists to allow python setup.py test to work.
 import os, sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ribbit.settings'
-test_dir = os.path.dirname(__file__)
+test_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, test_dir)
 
 from django.test.utils import get_runner
 from django.conf import settings
 
 def runtests():
-    test_runner = get_runner(settings)
-    failures = test_runner([], verbosity=1, interactive=True)
-    sys.exit(failures)
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(['ribbit.apps'])
+    sys.exit(bool(failures))
 
 if __name__ == '__main__':
     runtests()
