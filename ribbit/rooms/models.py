3d9094d1d2a6502cc579aecac8b2074a254731c8
giginet <giginet.net@gmail.com>
2013-12-14 01:38:09 +0900

    Rename field names of Room model

--- ribbit/rooms/models.py
+++ ribbit/rooms/models.py
@@ -14,12 +14,12 @@ class Room(models.Model):
     def icon_image_path(filename):
         return ''
 
-    name = models.CharField(max_length=128, verbose_name=_('Room'), null=False, blank=False)
+    title = models.CharField(max_length=128, verbose_name=_('Title'), null=False, blank=False)
     description = models.CharField(max_length=1024, verbose_name=_('Description'), null=True, blank=True)
     scope = models.CharField(choices=ROOM_SCOPE, verbose_name=_('Scope'), default='public', max_length=16)
-    icon_image = models.ImageField(verbose_name=_('Icon Image'), null=True, blank=True, upload_to=icon_image_path)
-    created_at = models.DateTimeField(auto_now=True, verbose_name=_('Created At'))
-    updated_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Updated At'))
+    icon_image = models.ImageField(verbose_name=_('Icon image'), null=True, blank=True, upload_to=icon_image_path)
+    created_at = models.DateTimeField(auto_now=True, verbose_name=_('Date created'))
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Date updated'))
 
     class Meta:
         verbose_name = 'Room'
