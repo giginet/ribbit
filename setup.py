2e0455396bff00852f3674be257352a2399c8c6a
giginet <giginet.net@gmail.com>
2013-12-18 08:20:33 +0900

    Revice the issue that user can't login when server is running via pulsar.
    - Merge this commits to pulsar
    
    https://github.com/quantmind/pulsar/issues/84

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(name='ribbit',
       url='https://github.com/giginet/ribbit',
       test_suite = "runtests.runtests",
       include_package_data = True,
-      install_requires=[
+      install_requires = [
         'PIL',
         'Django>=1.6.0',
         'factory_boy>=2.2.1',
         'pulsar',
         'easy-thumbnails',
         'honcho'
-      ]
+      ],
+      dependency_links = ['git+git@github.com:giginet/ribbit.git@c98f0bc99cfdd92827aa6de48fce1344b79e11e2#egg=ribbit-master',]
 )
