72c70021cb51b543cf121adc1869ceab24aa1a74
giginet <giginet.net@gmail.com>
2013-12-18 09:48:07 +0900

    Add spec for User#serialize

--- ribbit/apps/users/tests/model_tests.py
+++ ribbit/apps/users/tests/model_tests.py
@@ -23,6 +23,17 @@ class MessageTestCase(TestCase):
         message = UserFactory.create(username='kawaztan', screen_name='Kawaz tan')
         self.assertEqual(unicode(message), 'Kawaz tan(kawaztan)', '__unicode__() returns correct value')
 
+    def test_serialize(self):
+        """Test serialize() returns correct dict"""
+        user = UserFactory.create(twitter='@kawaztan', screen_name='Kawaz tan', username='kawaztan')
+        self.assertDictEqual(user.serialize(), {
+            'pk': user.pk,
+            'username': 'kawaztan',
+            'screen_name': 'Kawaz tan',
+            'twitter': '@kawaztan',
+            'avatar': ''
+        }, 'serialize() returns correct dict')
+
     def test_meta_class(self):
         """Test meta class attributes is correct"""
         self.assertEqual(User._meta.verbose_name.title(), 'User', 'Meta.verbose_name is correct')
