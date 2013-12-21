4b7ac44c8971a4cc9f31174aed12b2eeacfd4b26
giginet <giginet.net@gmail.com>
2013-12-21 14:53:56 +0900

    Add specs for RoomDetailView

--- ribbit/apps/rooms/factory_boy.py
+++ ribbit/apps/rooms/factory_boy.py
@@ -1,7 +1,10 @@
 import factory
-from models import Room
+from models import Room, Role
 
 from ribbit.apps.users.factory_boy import UserFactory
+
+class RoleFactory(factory.Factory):
+    FACTORY_FOR = Role
 
 class RoomFactory(factory.Factory):
     FACTORY_FOR = Room
--- ribbit/apps/rooms/tests/test_views.py
+++ ribbit/apps/rooms/tests/test_views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 
 from django.conf import settings
 
-from ribbit.apps.rooms.factory_boy import RoomFactory
+from ribbit.apps.rooms.factory_boy import RoomFactory, RoleFactory
 from ribbit.apps.users.factory_boy import UserFactory
 
 from ribbit.apps.users.models import User
@@ -12,6 +12,7 @@ from ribbit.apps.users.models import User
 class RoomViewTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='kawaztan', password='password')
+        self.another_user = User.objects.create_user(username='another', password='password')
 
     def test_authorized_user_can_view_room_creation(self):
         """Test authorized user can show RoomCreateView"""
@@ -27,3 +28,54 @@ class RoomViewTest(TestCase):
         response = c.get(url)
         self.assertRedirects(response, "%s?next=%s" % (settings.LOGIN_URL, url), status_code=302, target_status_code=301)
 
+    def test_not_authorized_user_cant_view_any_room(self):
+        """Test not authorized user can't access to any rooms."""
+        c = Client()
+        room = RoomFactory.create()
+        url = reverse('rooms_room_detail', args=(room.pk,))
+        response = c.get(url)
+        self.assertRedirects(response, "%s?next=%s" % (settings.LOGIN_URL, url), status_code=302, target_status_code=301)
+
+    def test_authorized_user_can_access_public_room(self):
+        """Test a authorized user can access to public rooms."""
+        c = Client()
+        self.assertTrue(c.login(username='kawaztan', password='password'))
+        room = RoomFactory.create(author=self.another_user)
+        room.save()
+        url = reverse('rooms_room_detail', args=(room.slug,))
+        response = c.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_permitted_user_cant_access_private_room(self):
+        """Test not permitted user can't access to the private room"""
+        c = Client()
+        self.assertTrue(c.login(username='kawaztan', password='password'))
+        room = RoomFactory.create(scope='private', author=self.another_user)
+        room.save()
+        url = reverse('rooms_room_detail', args=(room.slug,))
+        response = c.get(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_permitted_user_can_access_private_room(self):
+        """Test the permitted user can access to the private rooms."""
+        c = Client()
+        self.assertTrue(c.login(username='kawaztan', password='password'))
+        room = RoomFactory.create(scope='private', author=self.another_user)
+        room.save()
+        role = RoleFactory.create(room=room, user=self.user)
+        role.save()
+        url = reverse('rooms_room_detail', args=(room.slug,))
+        response = c.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rooms/room_detail.html')
+
+    def test_admin_user_can_access_owned_room(self):
+        """Test the admin users can access to the their managed rooms."""
+        c = Client()
+        self.assertTrue(c.login(username='kawaztan', password='password'))
+        room = RoomFactory.create(scope='private', author=self.user)
+        room.save()
+        url = reverse('rooms_room_detail', args=(room.slug,))
+        response = c.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'rooms/room_detail.html')
