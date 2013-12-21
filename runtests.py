924ed3fd64fa9f9bbae96c49440a6f46da7be9e1
giginet <giginet.net@gmail.com>
2013-12-21 15:29:13 +0900

    Fix testsuite settings

--- runtests.py
+++ runtests.py
@@ -10,7 +10,7 @@ from django.conf import settings
 def runtests():
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
-    failures = test_runner.run_tests(['ribbit.apps'])
+    failures = test_runner.run_tests(['ribbit'])
     sys.exit(bool(failures))
 
 if __name__ == '__main__':
